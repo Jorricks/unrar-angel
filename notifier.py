f71b9c9a050cbaa47303db49fc9c83236a920dd8
Jorricks <j.w.sleijster@student.tue.nl>
2017-12-04 23:32:55 +0100

    Fixed error in notifier

--- notifier.py
+++ notifier.py
@@ -10,11 +10,11 @@ class Notifier:
 
     def new_notification(self, watcher_uid):
         if self.config.get_config_watcher(watcher_uid, 'plex_on_or_off') == 1:
-            self.logger.py_logger('Notifier', 'Plex is next to be updated now that we had a successful file operation')
+            self.logger.info('Notifier', 'Plex is next to be updated now that we had a successful file operation')
             self.plex.update_library(watcher_uid)
 
         if self.config.get_config_watcher(watcher_uid, 'kodi_on_or_off') == 1:
-            self.logger.py_logger('Notifier', 'Kodi is next to be updated now that we had a successful file operation')
+            self.logger.info('Notifier', 'Kodi is next to be updated now that we had a successful file operation')
             try:
                 url = 'http://' + \
                       self.config.get_config_watcher(watcher_uid, 'kodi_user') + ':' + \
@@ -22,6 +22,6 @@ class Notifier:
                       self.config.get_config_watcher(watcher_uid, 'kodi_ip_port') + '/jsonrpc'
                 payload = {"jsonrpc": "2.0", "method": "VideoLibrary.Scan", "id": "mybash"}
                 requests.post(url, json=payload)
-                self.logger.py_logger('Kodi', 'Successful update')
+                self.logger.info('Kodi', 'Successful update')
             except requests.exceptions.ConnectionError:
-                self.logger.py_logger('Notifier', 'Error in updating Kodi. Could not connect')
+                self.logger.error('Notifier', 'Error in updating Kodi. Could not connect')
